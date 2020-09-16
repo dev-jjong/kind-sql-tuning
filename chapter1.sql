135d3465116bea316fab5e2f82dd400f65a7025c
dev-jjong <q_w@kakao.com>
2020-09-16 10:13:48 +0900

    chapter1 commit

--- chapter1.sql
+++ chapter1.sql
@@ -1,4 +1,10 @@
---1.1.1 구조적, 집합적, 선언적 질의 언어
+--1. SQL 처리 과정과 I/O
+--1.1 SQL 파싱과 최적화
+/* 1.1.1 구조적, 집합적, 선언적 질의 언어
+    SQL = Sructured Query Language = 구조적 질의 언어
+    SQL은 기본적으로 구조적(structured)이고 집합적(set-based)이고 선언적(declarative)인 질의 언어이다.
+    절자적인 프로시저를 만들어 내는 DBMS 내부엔진 = 옵티마이저
+*/
 
 -- 아래 2개의 테이블 생성, 테스트 데이터 insert
 create table DEPT(DEPTNO number primary key, DNAME varchar2(50) not null, LOC varchar2(50));
@@ -28,6 +34,32 @@ insert into EMP values(7902, 'FORD', 'ANALYST', 20);
 insert into EMP values(7934, 'MILLER', 'CLERK', 10);
 commit;
 select * from EMP;
+
+
+
+/*
+    1.1.2 SQL 최적화
+        [1] SQL 파싱
+            - 파싱 트리 생성 : SQL 문을 이루는 개별 구성요소를 분석해서 파싱 트리 생성
+            - Syntax 체크 : 문법적 오류가 없는지 확인
+            - Semantic 체크 : 의미상 오류가 없는지 확인
+
+        [2] SQL 최적화
+            SQL 옵티마이저는 미리 수집한 시스템 및 오브젝트 통계정보를 바탕으로 다양한 실행경로를 생성해서 비교한 후 가장 효율적인 하나를 선택한다.
+
+        [3] 로우 소스 생성
+            SQL 옵티마이저가 선택한 실행경로를 실제 실행 가능한 코드 또는 프로시저 형태로 포맷팅 하는 단계
+
+
+
+    1.1.3 SQL 옵티마이저
+
+
+
+*/
+
+
+
 
 
 create table t
